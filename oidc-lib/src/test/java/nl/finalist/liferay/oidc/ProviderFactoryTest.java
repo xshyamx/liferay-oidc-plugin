9ca078198a51f03fee4852f571c041d14c468f07
shyam <shyamsundar.shanmugam@wipro.com>
2018-12-11 10:29:43 +0530

    using parameterized tests

--- oidc-lib/src/test/java/nl/finalist/liferay/oidc/ProviderFactoryTest.java
+++ oidc-lib/src/test/java/nl/finalist/liferay/oidc/ProviderFactoryTest.java
@@ -4,40 +4,42 @@ import org.junit.Test;
 
 import nl.finalist.liferay.oidc.providers.AzureAD;
 import nl.finalist.liferay.oidc.providers.UserInfoProvider;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.assertTrue;
 
+@RunWith(Parameterized.class)
 public class ProviderFactoryTest {
 
-	@Test
-	public void testFactoryGenericProvider() {
-		UserInfoProvider openIdProvider = ProviderFactory.getOpenIdProvider("none");
-		assertTrue(openIdProvider.getClass() == UserInfoProvider.class);
+	@Parameters(name = "{0}")
+	public static Iterable<Object[]> params() {
+		return Arrays.asList(new Object[][]{
+				{"none", "none", UserInfoProvider.class},
+				{"generic match", "GENERIC", UserInfoProvider.class},
+				{"empty", "", UserInfoProvider.class},
+				{"azure case unmatched", "azure", AzureAD.class},
+				{"azure match", "AZURE", AzureAD.class},
+		});
 	}
-	
-	@Test
-	public void testFactoryGenericMatchProvider() {
-		UserInfoProvider openIdProvider = ProviderFactory.getOpenIdProvider("GENERIC");
-		assertTrue(openIdProvider.getClass() == UserInfoProvider.class);
-	}
-	
-	@Test
-	public void testFactoryGenericEmptyProvider() {
-		UserInfoProvider openIdProvider = ProviderFactory.getOpenIdProvider("");
-		assertTrue(openIdProvider.getClass() == UserInfoProvider.class);
-	}
-	
-	
-	@Test
-	public void testFactoryAzureMatchProvider() {
-		UserInfoProvider openIdProvider = ProviderFactory.getOpenIdProvider("AZURE");
-		assertTrue(openIdProvider instanceof AzureAD);
-	}
-	
-	@Test
-	public void testFactoryAzureCaseNotMatchProvider() {
-		UserInfoProvider openIdProvider = ProviderFactory.getOpenIdProvider("azure");
-		assertTrue(openIdProvider instanceof AzureAD);
+	private String name;
+	private String providerType;
+	private Class<?> expected;
+
+	public ProviderFactoryTest(String name, String providerType, Class<?> expected) {
+		this.name = name;
+		this.providerType = providerType;
+		this.expected = expected;
 	}
 
+	@Test
+	public void test() {
+		UserInfoProvider openIdProvider = ProviderFactory.getOpenIdProvider(providerType);
+		assertTrue(expected.isAssignableFrom(openIdProvider.getClass()));
+	}
+	
 }
